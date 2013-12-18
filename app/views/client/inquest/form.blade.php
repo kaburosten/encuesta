2c04ae5f10db2d0a29432e37672ad46d563406d1
kaburosten <kaburosten@gmail.com>
2013-12-18 09:59:10 -0600

    Nada

--- app/views/client/inquest/form.blade.php
+++ app/views/client/inquest/form.blade.php
@@ -9,16 +9,16 @@
 		<br>
 	<div class="form-group col-md-9">
 		    <legend>Cual fue la razon de que abandonaras lass clases?</legend>
-		    <label class="radio">
-					{{ Form::radio('question1', '1') }}
+		    <label class="radio horis" >
+					{{ Form::radio('1', '1') }}
 					Esta es la opcion numero uno para las respuestas de esta pregunta
 			</label>
-			<label class="radio">
-					{{ Form::radio('question1', '2') }}
+			<label class="radio horis">
+					{{ Form::radio('1', '2') }}
 					Esta es la opcion numero dos para las respuestas de esta pregunta
 			</label>
-			<label class="radio">
-					{{ Form::radio('question1', '3') }}
+			<label class="radio horis">
+					{{ Form::radio('1', '3') }}
 					Esta es la opcion numero tres para las respuestas de esta pregunta
 			</label>
 	</div>
@@ -26,159 +26,159 @@
 	<div class="form-group form-inline col-md-9">
 			<legend>Continuarás en el futuro cercano?</legend>
 			<label class="radio">
-					{{ Form::radio('question2', '1') }}
+					{{ Form::radio('2', '1') }}
 					Si
 			</label>
 			<label class="radio">
-					{{ Form::radio('question2', '2') }}
+					{{ Form::radio('2', '2') }}
 					No
 			</label>
 			<br><br>
 			<legend>De ser asi, continuarás en el mismo sistema de Institutos tecnologicos?</legend>
 			<label class="radio">
-					{{ Form::radio('question3', '1') }}
+					{{ Form::radio('3', '1') }}
 					Si
 			</label>
 			<label class="radio">
-					{{ Form::radio('question3', '2') }}
+					{{ Form::radio('3', '2') }}
 					No
 			</label>
 			<br><br>
 			<legend>Continuarás en la misma carrera?</legend>
 			<label class="radio">
-					{{ Form::radio('question4', '1') }}
+					{{ Form::radio('4', '1') }}
 					Si
 			</label>
 			<label class="radio">
-					{{ Form::radio('question4', '2') }}
+					{{ Form::radio('4', '2') }}
 					No
 			</label>
 			<br><br>
 			<legend>Actualmente trabajas?</legend>
 			<label class="radio">
-					{{ Form::radio('question5', '1') }}
+					{{ Form::radio('5', '1') }}
 					Si
 			</label>
 			<label class="radio">
-					{{ Form::radio('question6', '2') }}
+					{{ Form::radio('6', '2') }}
 					No
 			</label>
 			<br><br>
 			<legend>Tu sueldo alcanza para sostener tus estudios?</legend>
 			<label class="radio">
-					{{ Form::radio('question6', '1') }}
+					{{ Form::radio('6', '1') }}
 					Si
 			</label>
 			<label class="radio">
-					{{ Form::radio('question6', '2') }}
+					{{ Form::radio('6', '2') }}
 					No
 			</label>
 			<br><br>
 			<legend>Tus actividades academicas corresponden a sus actividades laborales?</legend>
 			<label class="radio">
-					{{ Form::radio('question7', '1') }}
+					{{ Form::radio('7', '1') }}
 					Si
 			</label>
 			<label class="radio">
-					{{ Form::radio('question7', '2') }}
+					{{ Form::radio('7', '2') }}
 					No
 			</label>
 			<br><br>
 			<legend>Tienes facilidad para asistir a clases?</legend>
 			<label class="radio">
-					{{ Form::radio('question8', '1') }}
+					{{ Form::radio('8', '1') }}
 					Si
 			</label>
 			<label class="radio">
-					{{ Form::radio('question8', '2') }}
+					{{ Form::radio('8', '2') }}
 					No
 			</label>
 			<br><br>
 			<legend>Actualmente cual es tu estado de salud?</legend>
 			<label class="radio">
-					{{ Form::radio('question9', '1') }}
+					{{ Form::radio('9', '1') }}
 					Bueno
 			</label>
 			<label class="radio">
-					{{ Form::radio('question9', '2') }}
+					{{ Form::radio('9', '2') }}
 					Regular
 			</label>
 			<label class="radio">
-					{{ Form::radio('question9', '2') }}
+					{{ Form::radio('9', '2') }}
 					Malo
 			</label>
 			<br><br>
 			<legend>Algun familiar cercano esta enfermo?</legend>
 			<label class="radio">
-					{{ Form::radio('question10', '1') }}
+					{{ Form::radio('10', '1') }}
 					Si
 			</label>
 			<label class="radio">
-					{{ Form::radio('question10', '2') }}
+					{{ Form::radio('10', '2') }}
 					No
 			</label>
 			<br><br>
 			<legend>Esa enfermedad te impide asistir a clases con regularidad?</legend>
 			<label class="radio">
-					{{ Form::radio('question11', '1') }}
+					{{ Form::radio('11', '1') }}
 					Si
 			</label>
 			<label class="radio">
-					{{ Form::radio('question11', '2') }}
+					{{ Form::radio('11', '2') }}
 					No
 			</label>
 			<br><br>
 			<legend>Va a dejar definitivamente los estudios?</legend>
 			<label class="radio">
-					{{ Form::radio('question12', '1') }}
+					{{ Form::radio('12', '1') }}
 					Si
 			</label>
 			<label class="radio">
-					{{ Form::radio('question12', '2') }}
+					{{ Form::radio('12', '2') }}
 					No
 			</label><br><br>
 			<legend>Cuentas con el apoyo moral de tu familia?</legend>
 			<label class="radio">
-					{{ Form::radio('question13', '1') }}
+					{{ Form::radio('13', '1') }}
 					Si
 			</label>
 			<label class="radio">
-					{{ Form::radio('question13', '2') }}
+					{{ Form::radio('13', '2') }}
 					No
 			</label>
 			<br><br>
 			<legend>Cuentas con apoyo economico para continuar estudiando?</legend>
 			<label class="radio">
-					{{ Form::radio('question14', '1') }}
+					{{ Form::radio('14', '1') }}
 					Si
 			</label>
 			<label class="radio">
-					{{ Form::radio('question14', '2') }}
+					{{ Form::radio('14', '2') }}
 					No
 			</label>
 			<br><br>
 			<legend>El motivo del abandono de estudios es por alguna cuestion academica?</legend>
 			<label class="radio">
-					{{ Form::radio('question15', '1') }}
+					{{ Form::radio('15', '1') }}
 					Si
 			</label>
 			<label class="radio">
-					{{ Form::radio('question15', '2') }}
+					{{ Form::radio('15', '2') }}
 					No
 			</label>
 			<br><br>
 			<legend>Haz buscado alguna solucion a este problema?</legend>
 			<label class="radio">
-					{{ Form::radio('question16', '1') }}
+					{{ Form::radio('16', '1') }}
 					Si
 			</label>
 			<label class="radio">
-					{{ Form::radio('question16', '2') }}
+					{{ Form::radio('16', '2') }}
 					No
 			</label>
 			<br><br>
 			<legend>Tienes algun motivo diferente para abandonar los estudios, describelo?</legend>
-			{{ Form::text('question17', null, array('class' => 'form-control', 'placeholder' => 'Describe tu motivo', 'rows' => '3'))}}
+			{{ Form::text('17', null, array('class' => 'form-control', 'placeholder' => 'Describe tu motivo', 'rows' => '3'))}}
 			<br><br>
 	</div>
 
